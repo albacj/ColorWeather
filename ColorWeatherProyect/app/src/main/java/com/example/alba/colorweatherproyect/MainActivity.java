895cee0ac148fe0aae37a1f49a1f8afd68eb8ed5
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-21 21:01:23 +0100

    Sección 15 clase 83 terminada: creación de dailyActivity, hourlyActivity y minutelyActivity. onclickListener para dailyActivity.

--- ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.alba.colorweatherproyect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView dailyWeatherTextView;
+    //private TextView hourlyWeatherTextView;
+    //private TextView minutelylyWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dailyWeatherTextView = (TextView) findViewById(R.id.dailyWeatherTextView);
+        dailyWeatherTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "click en daily Text View");
+            }
+        });
     }
 }
