f9d7a5844fe5ebefc193d7751319a9ee37f7594e
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-22 20:55:05 +0100

    Clase 89: Set Textos de clima actual y temperaturas.

--- ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,12 +16,24 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.iconImageView) ImageView iconImageView;
+    @BindView(R.id.descriptionTextView) TextView descriptionTextView;
+    @BindView(R.id.currentTempTextView) TextView currentTempTextView;
+    @BindView(R.id.highestTemptextView) TextView highestTempTextView;
+    @BindView(R.id.lowestTemptextView) TextView lowestTempTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        //iconImageView;
+        descriptionTextView.setText("Sunny Day");
+        currentTempTextView.setText("19");
+        highestTempTextView.setText("H: 25º");
+        lowestTempTextView.setText("L: 10º");
 
     }
 
