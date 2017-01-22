0902f8c95f9431eec173bdf2aa97fa8a0b4806f9
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-22 20:36:47 +0100

    Sección 16 Clase 86: Butterknife que hace click y lleva a daily forecast.

--- ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
@@ -7,28 +7,30 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
-    private TextView dailyWeatherTextView;
-    //private TextView hourlyWeatherTextView;
-    //private TextView minutelylyWeatherTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        dailyWeatherTextView = (TextView) findViewById(R.id.dailyWeatherTextView);
-        dailyWeatherTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent dailyActivityIntent = new Intent(MainActivity.this, DailyWeatherActivity.class);
-                startActivity(dailyActivityIntent);
-
-            }
-        });
     }
+
+    @OnClick(R.id.dailyWeatherTextView)
+    public void dailyWeatherClick(){
+
+        Intent dailyActivityIntent = new Intent(MainActivity.this, DailyWeatherActivity.class);
+        startActivity(dailyActivityIntent);
+
+
+    }
+
 }
