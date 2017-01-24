a08be48e47c7e12e0846885b73c35787ce0daa6c
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-24 12:21:00 +0100

    Sección 17 Clase 97: Librería volley agregada y primera petición a internet exitosa.

--- ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/MainActivity.java
@@ -9,6 +9,13 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -46,6 +53,30 @@ public class MainActivity extends AppCompatActivity {
         currentTempTextView.setText(currentWeather.getCurrentTemperature());
         highestTempTextView.setText(currentWeather.getHighestTemperature());
         lowestTempTextView.setText(currentWeather.getLowestTemperature());
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                // Display the first 500 characters of the response string.
+                Log.d(TAG, "Response is: "+ response.substring(0,500));
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "That didn't work");
+            }
+        });
+
+    // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
     }
 
