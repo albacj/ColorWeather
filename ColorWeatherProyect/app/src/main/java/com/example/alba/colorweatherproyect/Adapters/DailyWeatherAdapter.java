451a3ef21ce0c6ab5c0becd69c2882eda4d53609
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-30 17:16:25 +0100

    Sección 18 Clase 113: Implementando DailyWeatherActivity.

--- ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/Adapters/DailyWeatherAdapter.java
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/Adapters/DailyWeatherAdapter.java
@@ -1,8 +1,16 @@
 package com.example.alba.colorweatherproyect.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.alba.colorweatherproyect.Day;
+import com.example.alba.colorweatherproyect.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Alba on 30/01/2017.
@@ -10,14 +18,22 @@ import android.widget.BaseAdapter;
 
 public class DailyWeatherAdapter extends BaseAdapter{
 
+    ArrayList<Day> days;
+    Context context;
+
+    public DailyWeatherAdapter(Context context, ArrayList<Day> days){
+        this.context = context;
+        this.days = days;
+    }
+
     @Override
     public int getCount() { // cuántos elementos hay en nuestros datos
-        return 0;
+        return days.size();
     }
 
     @Override
     public Object getItem(int position) { // Posiciones a "rendear"
-        return null;
+        return days.get(position);
     }
 
     @Override
@@ -27,7 +43,20 @@ public class DailyWeatherAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) { //Como vamos a rendear la vista y a mapear los datos
-        return null;
+
+        Day day = days.get(position);
+
+        convertView = LayoutInflater.from(context).inflate(R.layout.daily_list_item, null);
+
+        TextView dayTitle = (TextView) convertView.findViewById(R.id.dailyListTitle);
+        TextView dayDescription = (TextView) convertView.findViewById(R.id.dailyListDescription);
+        TextView dayRainProbability = (TextView) convertView.findViewById(R.id.dailyListProbability);
+
+        dayTitle.setText(day.getDayName());
+        dayDescription.setText(day.getWeatherDescription());
+        dayRainProbability.setText(day.getRainProbability());
+
+        return convertView;
     }
 
 }
--- ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/DailyWeatherActivity.java
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/DailyWeatherActivity.java
@@ -5,6 +5,8 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
+import com.example.alba.colorweatherproyect.Adapters.DailyWeatherAdapter;
+
 import java.util.ArrayList;
 
 public class DailyWeatherActivity extends ListActivity { //ListActivity ayuda a trabajar con listViews
@@ -14,16 +16,23 @@ public class DailyWeatherActivity extends ListActivity { //ListActivity ayuda a 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_weather);
 
-        ArrayList<String> daysArray = new ArrayList<String>();
-        daysArray.add("Lunes");
-        daysArray.add("Martes");
-        daysArray.add("Miércoles");
-        daysArray.add("Jueves");
-        daysArray.add("Viernes");
-        daysArray.add("Sábado");
-        daysArray.add("Domingo");
+        ArrayList<Day> days = new ArrayList<Day>();
 
-        ArrayAdapter<String> daysAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,daysArray);
-        setListAdapter(daysAdapter);
+        for(int i = 0; i < 500; i++){
+
+            Day day = new Day();
+            day.setDayName("Monday");
+            day.setWeatherDescription("It is going to rain");
+            day.setRainProbability("Rain probability: 99%");
+
+            days.add(day);
+
+        }
+
+
+
+        DailyWeatherAdapter dailyWeatherAdapter = new DailyWeatherAdapter(this,days);
+
+        setListAdapter(dailyWeatherAdapter);
     }
 }
