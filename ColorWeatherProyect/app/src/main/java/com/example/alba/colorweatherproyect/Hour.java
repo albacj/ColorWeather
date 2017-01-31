c8a2fb37ea41582286037383af025d513d4831b7
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-31 13:07:29 +0100

    Sección 19 Clase 118: Creación de la clase Hora.

---
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/Hour.java
@@ -0,0 +1,28 @@
+package com.example.alba.colorweatherproyect;
+
+/**
+ * Created by Alba on 31/01/2017.
+ */
+
+public class Hour {
+
+    private String title;
+    private String weatherDescription;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+}
