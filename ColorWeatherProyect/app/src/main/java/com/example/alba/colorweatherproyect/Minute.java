b43fb4eac71dcb486c093e219c6b44388b90d7d4
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-02-01 12:23:14 +0100

    Sección 19 Clase 122: Clase Minute creada.

---
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/Minute.java
@@ -0,0 +1,28 @@
+package com.example.alba.colorweatherproyect;
+
+/**
+ * Created by Alba on 01/02/2017.
+ */
+
+public class Minute {
+
+    private String title;
+    private String rainProbability;
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+}
