d894b7ed20cce39678017ca813cf24b87bb91752
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-02-12 16:46:40 +0100

    Sección 22 Clase 135: Parcelable Hour.

--- ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/Hour.java
+++ ColorWeatherProyect/app/src/main/java/com/example/alba/colorweatherproyect/Hour.java
@@ -1,13 +1,37 @@
 package com.example.alba.colorweatherproyect;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Alba on 31/01/2017.
  */
 
-public class Hour {
+public class Hour implements Parcelable{
 
     private String title;
     private String weatherDescription;
+
+    public Hour(){
+        
+    }
+
+    protected Hour(Parcel in) {
+        title = in.readString();
+        weatherDescription = in.readString();
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel in) {
+            return new Hour(in);
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -25,4 +49,16 @@ public class Hour {
         this.weatherDescription = weatherDescription;
     }
 
+    @Override
+    public int describeContents() {
+        return 0; //No lo necesito
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeString(title);
+        dest.writeString(weatherDescription);
+
+    }
 }
